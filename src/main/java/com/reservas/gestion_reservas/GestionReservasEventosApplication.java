9fe4f8ba4ded22dcd8f3eb22253f89e9746fda74
santiago <santiago.ospina1@utp.edu.co>
2024-09-26 10:40:20 -0500

    Initial commit

---
+++ src/main/java/com/reservas/gestion_reservas/GestionReservasEventosApplication.java
@@ -0,0 +1,13 @@
+package com.reservas.gestion_reservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionReservasEventosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionReservasEventosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reservas/gestion_reservas/GestionReservasEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservas.gestion_reservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionReservasEventosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
